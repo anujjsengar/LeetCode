964a8b59244e3a0727251d4750a09a2acf3182c2
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-02-16 01:03:16 +0530

    Create 523.Continuous Subarray Sum.java

---
+++ 523.Continuous Subarray Sum.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+
+        Map<Integer, Integer> remainderMap = new HashMap<>();
+        remainderMap.put(0, -1); // Initialize with a remainder of 0 at index -1.
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (k != 0) {
+                sum = sum % k;
+            }
+
+            if (remainderMap.containsKey(sum)) {
+                // Check if the subarray has at least two elements.
+                if (i - remainderMap.get(sum) > 1) {
+                    return true;
+                }
+            } else {
+                remainderMap.put(sum, i);
+            }
+        }
+
+        return false;
+    }
+}
+/*
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+Note that:
+
+A subarray is a contiguous part of the array.
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+Example 2:
+
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+Example 3:
+
+Input: nums = [23,2,6,4,7], k = 13
+Output: false
+  */
