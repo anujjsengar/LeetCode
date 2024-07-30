de745ece3a0f4afb500f4338d2183ff00625ca2d
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-07-30 12:43:28 +0530

    Time: 181 ms (13.25%), Space: 45.4 MB (10.12%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,31 +1,44 @@
-public class Info {
-    int diameter;
-    int height;
-
-    public Info(int diameter, int height) {
-        this.diameter = diameter;
-        this.height = height;
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public Info fun(TreeNode root) {
+    public int diameterOfBinaryTree(TreeNode root) {
         if (root == null) {
-            return new Info(0, 0);
+            return 0;
         }
-        Info leftData = fun(root.left);
-        Info rightData = fun(root.right);
-
-        int currDiameter = Math.max(Math.max(leftData.diameter, rightData.diameter),
-                leftData.height + rightData.height);
-        int currHeight = Math.max(leftData.height, rightData.height) + 1;
-
-        return new Info(currDiameter, currHeight);
-
+        return calc(root) - 1;
     }
 
-    public int diameterOfBinaryTree(TreeNode root) {
-        Info ans = fun(root);
-        return ans.diameter;
+    public int calc(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        int diameter = leftHeight + rightHeight + 1;
+        int leftDiameter = calc(root.left);
+        int rightDiameter = calc(root.right);
+        return Math.max(diameter, Math.max(leftDiameter, rightDiameter));
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
     }
 }
