0b562df9e82736a63c743b184d6167ff26b82311
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-10-16 20:46:10 +0530

    Time: 2 ms (93.92%), Space: 56.1 MB (21.39%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,43 +1,28 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        if(bills[0]>5){
+     int five=0;
+     int ten=0;
+     for(int i:bills){
+        if(i==5){
+            five++;
+        }
+        if(i==10){
+            ten++;
+            five--;
+        }
+        if(i==20){
+            if(ten>0){
+                ten--;
+                five--;
+            }
+            else{
+                five-=3;
+            }
+        }
+        if(five<0){
             return false;
         }
-        Map<Integer,Integer> li=new HashMap<>();
-        li.put(5,1);
-        li.put(10,0);
-        for(int i=1;i<bills.length;i++){
-            if(bills[i]==5){
-            li.put(5,li.get(5)+1);
-            }
-            if(bills[i]==10){
-                if(li.get(5)==0){
-                    return false;
-                }
-                else{
-                    li.put(5,li.get(5)-1);
-                    li.put(10,li.get(10)+1);
-                }
-            }
-            if(bills[i]==20){
-                if(li.get(5)==0){
-                    return false;
-                }
-                else{
-                    li.put(5,li.get(5)-1);
-                    if(li.get(10)>0){
-                        li.put(10,li.get(10)-1);
-                    }
-                    else{
-                        if(li.get(5)<2){
-                            return false;
-                        }
-                        else{
-                            li.put(5,li.get(5)-2);
-                        }
-                    }
-                }
-            }
-        }return true;
+     }
+     return true;   
     }
 }
