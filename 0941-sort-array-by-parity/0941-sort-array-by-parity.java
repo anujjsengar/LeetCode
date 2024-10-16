9534862fb2e3fcefc283a807389b09bbc4ab496d
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-10-16 19:35:09 +0530

    Time: 1 ms (52.76%), Space: 44.9 MB (46.22%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,31 +1,25 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        /*PriorityQueue<Integer>pq=new PriorityQueue<>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer a,Integer b){
-                if(a%2!=0 && b%2==0){
-                    return 1;
-                }
-                return 0;
+        int left=0;
+        int right=nums.length-1;
+        while(left<right){
+            if(nums[left]%2!=0 && nums[right]%2==0){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right--;
             }
-        });
-        int index=0;
-        while(!pq.isEmpty()){
-            nums[index]=pq.poll();
-            index++;
-        }
-        return nums;*/
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2!=0){
-               for(int j=i+1;j<nums.length;j++){
-                if(nums[j]%2==0){
-                    int temp=nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                }
-               } 
+            else if(nums[left]%2==0 && nums[right]%2==0){
+                left++;
+            }
+            else if(nums[left]%2!=0 && nums[right]%2!=0){
+                right--;
+            }
+            else{
+                left++;
             }
         }
-        return nums;
+    return nums;
     }
 }
