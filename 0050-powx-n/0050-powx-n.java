f6750bb5c3baf57c3883c666ea4d0f6ded68e87f
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-11-06 18:56:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n>0){
+            return x*myPow(x,n-1);
+        }
+        else{
+            return 1/x*myPow(x,n+1);
+        }
+    }
+}
