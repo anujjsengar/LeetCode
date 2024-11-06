311a78339bf145730f64ec9d73dd7792406f9cd4
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-11-06 20:13:39 +0530

    Time: 6 ms (41.44%), Space: 46 MB (16.14%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -5,6 +5,7 @@ class Solution {
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1){
                     check(grid,i,j);
+                    break;
                 }
             }
         }
