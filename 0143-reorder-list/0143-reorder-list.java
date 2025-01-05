9b9ddda6bc9beb5126d21dd448da0e0a92917b01
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-01-05 12:50:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,45 +10,6 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-
-        // step 1 : find the middle node
-        ListNode slow = head;
-        ListNode fast = head.next;
-
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode mid = slow;    
-
-        // step 2 : reverse the second half 
-
-        ListNode curr = mid.next;
-        mid.next = null;
-        ListNode prev = null;
-        ListNode next;
-
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
         
-
-        //step 3 : Alteration code
-        ListNode leftHead = head;
-        ListNode rightHead = prev;
-        ListNode nextL , nextR;
-
-        while(leftHead != null && rightHead != null){
-            nextL = leftHead.next;
-            leftHead.next = rightHead;
-            nextR = rightHead.next;
-            rightHead.next = nextL; 
-
-            rightHead = nextR;
-            leftHead = nextL;
-        }
     }
 }
