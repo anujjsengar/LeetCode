523753ef8e1d7be2a0c6a6448e3edd5bf555cd2f
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-02-15 18:21:54 +0530

    Time: 17 ms (76.93%), Space: 46.2 MB (68.4%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -16,17 +16,29 @@
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
         List<Integer> li=new ArrayList<>();
-        dfs(root1,li);
-        dfs(root2,li);
+        dfs(root1,root2,li);
         Collections.sort(li);
         return li;
     }
-    public void dfs(TreeNode root,List<Integer> li){
-        if(root==null){
+    public void dfs(TreeNode root1,TreeNode root2,List<Integer> li){
+        if(root1==null && root2==null){
             return ;
         }
-        dfs(root.left,li);
-        li.add(root.val);
-        dfs(root.right,li);
+        if(root1==null){
+            li.add(root2.val);
+            dfs(root1,root2.left,li);
+            dfs(root1,root2.right,li);
+        }
+        if(root2==null){
+            li.add(root1.val);
+            dfs(root1.left,root2,li);
+            dfs(root1.right,root2,li);
+        }
+        if(root1!=null && root2!=null){
+            li.add(root1.val);
+            li.add(root2.val);
+            dfs(root1.left,root2.left,li);
+            dfs(root1.right,root2.right,li);
+        }
     }
 }
