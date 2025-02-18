dc832613b1db9be22f283d347f26c69ff4d67c74
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-02-18 21:43:29 +0530

    Time: 33 ms (9.29%), Space: 51.2 MB (30.09%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -9,18 +9,19 @@ class Solution {
         return solve(text1,text2,0,0,dp);
     }
     public int solve(String text1,String text2,int i,int j,int[][] dp){
-        if(i==text1.length() || j==text2.length()){
+        if(text1.length()==i || text2.length()==j){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
+        int count=0;
         if(text1.charAt(i)==text2.charAt(j)){
-            dp[i][j]=1+solve(text1,text2,i+1,j+1,dp);
+            count=1+solve(text1,text2,i+1,j+1,dp);
         }
         else{
-            dp[i][j]=Math.max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
+            count=Math.max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
         }
-        return dp[i][j];
+        return dp[i][j]=count;
     }
 }
