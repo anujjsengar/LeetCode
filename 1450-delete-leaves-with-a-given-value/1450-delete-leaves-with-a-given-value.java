50fbb9e3def98b9a4aa5b461cbfff1ab7a347efd
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-04-26 18:31:43 +0530

    Time: 0 ms (100%), Space: 44.5 MB (30.4%) - LeetHub

--- 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.java
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.java
@@ -18,27 +18,11 @@ class Solution {
         if(root==null){
             return null;
         }
-        remove(root,target,root);
+        root.left=removeLeafNodes(root.left,target);
+        root.right=removeLeafNodes(root.right,target);
         if(root.left==null && root.right==null && root.val==target){
             return null;
         }
         return root;
-    }
-    public void remove(TreeNode root,int target,TreeNode prev){
-        if(root==null){
-            return ;
-        }
-        remove(root.left,target,root);
-        remove(root.right,target,root);
-        if(root.val==target){
-            if(prev.right==root && root.left==null && root.right==null){
-                prev.right=null;
-                return ;
-            }
-            if(prev.left==root && root.right==null && root.left==null){
-                prev.left=null;
-                return ;
-            }
-        }
     }
 }
